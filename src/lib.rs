1163cae232ee1524283b9fa5ed0ad9ceea40d2f4
willthamic <will.hamic@gmail.com>
2020-02-27 01:28:23 -0600

    added basic proccessing

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,26 @@
 use std::error::Error;
 use std::fs;
+use std::path::PathBuf;
 
 pub struct Config {
-    pub source_filename: String,
-    pub output_filename: String,
+    pub source_path: PathBuf,
+    pub output_path: PathBuf,
+}
+
+struct ILine {
+    raw: String,
+    label: Option<String>,
+    opcode: Opcode,
+    comment: Option<String>,
+}
+
+enum Opcode {
+    LD,
+    LDR,
+    ST,
+    STR,
+    LA,
+    LAR,
 }
 
 impl Config {
@@ -12,17 +29,59 @@ impl Config {
             return Err("not enough arguments");
         }
 
-        let source_filename = args[1].clone();
-        let output_filename = "source.bin".to_string();
+        let source_path = PathBuf::from(args[1].clone());
+        let output_path = source_path.with_extension("bin");
 
-        Ok(Config { source_filename, output_filename })
+        Ok(Config { source_path, output_path })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.source_filename)?;
+    let contents = fs::read_to_string(config.source_path)?;
+    let lines = contents.lines();
 
-    println!("File contents:\n{}", contents);
+    let mut instructions = Vec::<ILine>::new();
+
+    for line in lines {
+        match process_line(line.to_string()) {
+            Some(Ok(x)) => instructions.push(x),
+            Some(Err(x)) => eprintln!("{}", x),
+            None => (),
+        }
+    }
+
+    for inst in instructions {
+        println!("{}", inst.)
+    }
 
     Ok(())
+}
+
+fn process_line(line: String) -> Option<Result<ILine, &'static str>> {
+    let line = line.trim();
+    let comment_pos = line.find(';');
+    let line_trimmed = match comment_pos {
+        Some(x) => &line[..x],
+        None    => &line,
+    };
+    let empty = line_trimmed.is_empty();
+    if !empty {
+        let comment = match comment_pos {
+            Some(x) => Some(line[x..].to_string()),
+            None    => None,
+        };
+        let label_pos = line_trimmed.find(':');
+        let label = match label_pos {
+            Some(x) => Some(line[..x].to_string()),
+            None    => None,
+        };
+        Some(Ok(ILine{
+            raw: line.to_string(),
+            label: label,
+            opcode: Opcode::LD,
+            comment: comment,
+        }))
+    } else {
+        None
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    println!("Source file: {}", config.source_filename);
-    println!("Output file: {}", config.output_filename);
     
     if let Err(e) = orange_assembler::run(config) {
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
 
         process::exit(1);
     }
