91845c2217b1257e384f3447697244c0783d6f2d
willthamic <will.hamic@gmail.com>
2020-02-26 21:17:28 -0600

    Moved source.asm and added file input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let source_file = &args[1];
+
+    let contents = fs::read_to_string(source_file)
+        .expect("Something went wrong reading the file");
+    
+    
+    println!("Source file: {}", source_file);
+    println!("File contents:\n{}", contents);
 }
