af1da1b6fe4eb735400272161c58adcc0a83a6f8
willthamic <will.hamic@gmail.com>
2020-03-01 19:32:45 -0600

    added more instructions and expanded testing

--- src/lib.rs
+++ src/lib.rs
@@ -20,11 +20,13 @@ struct ILine<'a> {
     comment: Option<&'a str>,
 }
 
+#[derive(Debug, PartialEq)]
 struct Instruction {
     opcode: Opcode,
     args: Arguments,
 }
 
+#[derive(Debug, PartialEq)]
 struct Arguments {
     ra: Option<usize>,
     rb: Option<usize>,
@@ -34,20 +36,20 @@ struct Arguments {
     c3: Option<usize>,
 }
 
-impl fmt::Debug for Instruction {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f, "{}{:?}{:?}{:?}{:?}{:?}{:?}", 
-            self.opcode, 
-            self.args.ra, 
-            self.args.rb, 
-            self.args.rc, 
-            self.args.c1, 
-            self.args.c2, 
-            self.args.c3
-        )
-    }
-}
+// impl fmt::Debug for Instruction {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         write!(
+//             f, "{}{:?}{:?}{:?}{:?}{:?}{:?}", 
+//             self.opcode, 
+//             self.args.ra, 
+//             self.args.rb, 
+//             self.args.rc, 
+//             self.args.c1, 
+//             self.args.c2, 
+//             self.args.c3
+//         )
+//     }
+// }
 
 #[derive(EnumString, Display, Debug, PartialEq)]
 enum Opcode {
@@ -80,9 +82,11 @@ enum Opcode {
 
     // op_rb_rc_c3
     BR,
+    // BRNV,
+
 
     // op_ra_rb_rc_c3
-    BRL,
+    // BRL,
 
     // op_ra_rb_n
     SHR,
@@ -187,27 +191,45 @@ fn process_instruction(inst: &str) -> Result<Option<Instruction>, &'static str> 
             Ok(x) => x,
             Err(_) => return Err("Opcode not found"),
         };
-        let args = match opcode {
-            Opcode::LA   => process_op_ra_rb_c2(inst, opcode),
-            Opcode::NOP  => process_op(inst, opcode),
-            _ => Err("temp"),
+        let args = match process_args(inst, &opcode) {
+            Ok(x) => x,
+            Err(x) => return Err(x),
         };
-        match inst {
-            Ok(x) => Ok(Some(Instruction{
-                opcode,
-                args,
-            })),
-            Err(x) => Err(x)
-        }
+        Ok(Some(Instruction{
+            opcode,
+            args,
+        }))
     }
 }
 
-fn process_args (inst: &str, opcode: Opcode) -> Result<Arguments, &'static str> {
+fn process_args (inst: &str, opcode: &Opcode) -> Result<Arguments, &'static str> {
     match opcode {
-        Opcode::LA   => process_op_ra_rb_c2(inst, opcode),
-        Opcode::NOP  => process_op(inst, opcode),
-        _ => Err("temp"),
-    };
+        Opcode::NOP | Opcode::STOP 
+            => process_op(inst, opcode),
+
+        Opcode::ADD | Opcode::SUB | Opcode::AND | Opcode::OR 
+            => process_op_ra_rb_rc(inst, &opcode),
+
+        Opcode::ADDI | Opcode::ANDI | Opcode::ORI | Opcode::LD | Opcode::ST | Opcode::LA
+            => process_op_ra_rb_c2(inst, &opcode),
+
+        Opcode::LDR | Opcode::STR | Opcode::LAR
+            => process_op_ra_c1(inst, &opcode),
+        
+        Opcode::NEG | Opcode::NOT
+            => process_op_ra_rc(inst, &opcode),
+
+        Opcode::BR
+            => process_branch(inst, &opcode),
+
+        Opcode::SHR | Opcode::SHRA | Opcode::SHL | Opcode::SHC
+            => process_op_ra_rb_n(inst, &opcode),
+        // _ => Err("temp"),
+    }
+}
+
+fn process_branch (inst: &str, opcode: &Opcode) -> Result<Arguments, &'static str> {
+    Err("NOT IMPLEMENTED TEMP")
 }
 
     // OP,
@@ -217,12 +239,13 @@ fn process_args (inst: &str, opcode: Opcode) -> Result<Arguments, &'static str> 
     // OP_RA_RC,
     // OP_RB_RC_C3,
     // OP_RA_RB_RC_C3,
-    // OP_RA_RB_N,
+    // OP_RA_RB_C3,
 
-fn process_op (inst: &str, opcode: Opcode) -> Result<Arguments, &'static str> {
+fn process_op (inst: &str, opcode: &Opcode) -> Result<Arguments, &'static str> {
+    let inst = inst.trim();
     match inst.is_empty() {
-        true => Err("why are there arguments"),
-        false => {
+        false => Err("why are there arguments"),
+        true => {
             Ok(Arguments {
                 ra: None,
                 rb: None,
@@ -233,17 +256,50 @@ fn process_op (inst: &str, opcode: Opcode) -> Result<Arguments, &'static str> {
             })
         },
     }
-    
 }
 
-fn process_op_ra_rb_c2 (inst: &str, opcode: Opcode) -> Result<Arguments, &'static str> {
+fn process_op_ra_rb_rc (inst: &str, opcode: &Opcode) -> Result<Arguments, &'static str> {
     let (ra, inst) = match inst.find(',') {
-        Some(x) => (&inst[..x], &inst[x..]),
-        None => return Err("no comma"),
+        Some(x) => (&inst[..x], &inst[(x+1)..]),
+        None => return Err("no comma temp"),
     };
-    let rb = match (inst.find('('), inst.find('(')) {
-        (Some(x), Some(y)) if (x < y) => &inst[x..y],
-        _ => "",
+    let (rb, inst) = match inst.find(',') {
+        Some(x) => (&inst[..x], &inst[(x+1)..]),
+        None => return Err("no comma temp"),
+    };
+    let rc = inst;
+
+    let ra = match register_string_parse(ra) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
+    let rb = match register_string_parse(rb) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
+    let rc = match register_string_parse(rc) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
+
+    Ok(Arguments {
+        ra: ra,
+        rb: rb,
+        rc: rc,
+        c1: None,
+        c2: None,
+        c3: None,
+    })
+}
+
+fn process_op_ra_rb_c2 (inst: &str, opcode: &Opcode) -> Result<Arguments, &'static str> {
+    let (ra, inst) = match inst.find(',') {
+        Some(x) => (&inst[..x], &inst[(x+1)..]),
+        None => return Err("no comma temp"),
+    };
+    let (c2, rb) = match (inst.find('('), inst.find(')')) {
+        (Some(x), Some(y)) if (x < y) => (&inst[..x], &inst[(x+1)..y]),
+        _ => (inst, "0"),
     };
 
     let ra = match register_string_parse(ra) {
@@ -254,12 +310,93 @@ fn process_op_ra_rb_c2 (inst: &str, opcode: Opcode) -> Result<Arguments, &'stati
         Ok(x) => Some(x),
         Err(x) => return Err(x),
     };
-
+    let c2 = match parse_constant(c2) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
 
     Ok(Arguments {
         ra: ra,
         rb: rb,
         rc: None,
+        c1: None,
+        c2: c2,
+        c3: None,
+    })
+}
+
+fn process_op_ra_c1 (inst: &str, opcode: &Opcode) -> Result<Arguments, &'static str> {
+    let (ra, inst) = match inst.find(',') {
+        Some(x) => (&inst[..x], &inst[(x+1)..]),
+        None => return Err("no comma temp"),
+    };
+    let c1 = inst;
+
+    let ra = match register_string_parse(ra) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
+    let c1 = match parse_constant(c1) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
+
+    Ok(Arguments {
+        ra: ra,
+        rb: None,
+        rc: None,
+        c1: c1,
+        c2: None,
+        c3: None,
+    })
+}
+
+fn process_op_ra_rc (inst: &str, opcode: &Opcode) -> Result<Arguments, &'static str> {
+    let (ra, inst) = match inst.find(',') {
+        Some(x) => (&inst[..x], &inst[(x+1)..]),
+        None => return Err("no comma temp"),
+    };
+    let rc = inst;
+
+    let ra = match register_string_parse(ra) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
+    let rc = match register_string_parse(rc) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
+
+    Ok(Arguments {
+        ra: ra,
+        rb: None,
+        rc: rc,
+        c1: None,
+        c2: None,
+        c3: None,
+    })
+}
+
+fn process_op_ra_rb_n (inst: &str, opcode: &Opcode) -> Result<Arguments, &'static str> {
+    let (ra, inst) = match inst.find(',') {
+        Some(x) => (&inst[..x], &inst[(x+1)..]),
+        None => return Err("no comma temp"),
+    };
+    let rc = inst;
+
+    let ra = match register_string_parse(ra) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
+    let rc = match register_string_parse(rc) {
+        Ok(x) => Some(x),
+        Err(x) => return Err(x),
+    };
+
+    Ok(Arguments {
+        ra: ra,
+        rb: None,
+        rc: rc,
         c1: None,
         c2: None,
         c3: None,
@@ -267,17 +404,33 @@ fn process_op_ra_rb_c2 (inst: &str, opcode: Opcode) -> Result<Arguments, &'stati
 }
 
 fn register_string_parse(reg: &str) -> Result<usize, &'static str> {
+    let reg = reg.trim();
     if reg.len() < 2 || reg.len() > 3  {
-        return Err("Incorrect register formatting");
+        println!("e:[{}]", reg);
+        return Err("Incorrect register formatting (too many characters)");
     }
 
-    let (a, b) = (&reg[0..0], &reg[1..]);
+    let (a, b) = (reg.chars().next().unwrap(), &reg[1..]);
+
+    if a != 'r' {
+        return Err("Incorrect register formatting (does not start with 'r')");
+    }
 
     let ret = b.parse::<usize>();
 
     match ret {
+        Ok(x) if x < 32 => Ok(x),
+        Ok(x) => Err("Invalid register"),
+        Err(_) => Err("Incorrect register formatting (could not parse index)"),
+    }
+}
+
+fn parse_constant(con: &str) -> Result<usize, &'static str> {
+    let con = con.trim();
+    let con = con.parse::<usize>();
+    match con {
         Ok(x) => Ok(x),
-        Err(x) => Err("Incorrect register formatting"),
+        Err(_) => Err("Could not parse constant"),
     }
 }
 
@@ -287,13 +440,132 @@ mod tests {
 
     #[test]
     fn process_op_test() {
-        let result = process_op("nop", Opcode::NOP).unwrap();
-        assert_eq!(result.ra, None);
-        assert_eq!(result.rb, None);
-        assert_eq!(result.rc, None);
-        assert_eq!(result.c1, None);
-        assert_eq!(result.c2, None);
-        assert_eq!(result.c3, None);
+        let tests = [
+            ("", &Opcode::NOP, 
+            Arguments{ra: None, rb: None, rc: None, c1: None, c2: None, c3: None}),
+            ("", &Opcode::STOP, 
+            Arguments{ra: None, rb: None, rc: None, c1: None, c2: None, c3: None}),
+            (" ", &Opcode::NOP, 
+            Arguments{ra: None, rb: None, rc: None, c1: None, c2: None, c3: None}),
+        ];
+        for test in &tests {
+            let result = process_op(test.0, test.1).unwrap();
+            assert_eq!(result, test.2);
+        }
+
+        let invalid_tests = [
+            ("test", &Opcode::NOP)
+        ];
+        for test in &invalid_tests {
+            let result = process_op(test.0, test.1);
+            assert!(result.is_err());
+        }
+    }
+
+    #[test]
+    fn process_op_ra_rb_rc_test() {
+        let tests = [
+            ("r1, r2, r3", &Opcode::ADD, 
+            Arguments{ra: Some(1), rb: Some(2), rc: Some(3), c1: None, c2: None, c3: None}),
+            ("  r23,   r6,r11 ", &Opcode::SUB, 
+            Arguments{ra: Some(23), rb: Some(6), rc: Some(11), c1: None, c2: None, c3: None}),
+        ];
+        for test in &tests {
+            let result = process_op_ra_rb_rc(test.0, test.1).unwrap();
+            assert_eq!(result, test.2);
+        }
+
+        let invalid_tests = [
+            ("test", &Opcode::NOP),
+            ("r1, r3, r3123", &Opcode::NOP),
+            ("r2,,r3 r3", &Opcode::NOP),
+            ("a3,b3", &Opcode::NOP),
+        ];
+        for test in &invalid_tests {
+            let result = process_op_ra_rb_rc(test.0, test.1);
+            assert!(result.is_err(), format!("failed with [{}]", test.0));
+        }
+    }
+
+    #[test]
+    fn process_op_ra_rb_c2_test() {
+        let tests = [
+            ("r1, 5(r2)", &Opcode::LD, 
+            Arguments{ra: Some(1), rb: Some(2), rc: None, c1: None, c2: Some(5), c3: None}),
+            ("  r23,   16 ( r11 ) ", &Opcode::ST, 
+            Arguments{ra: Some(23), rb: Some(11), rc: None, c1: None, c2: Some(16), c3: None}),
+            ("  r23 ,16( r11)", &Opcode::ST, 
+            Arguments{ra: Some(23), rb: Some(11), rc: None, c1: None, c2: Some(16), c3: None}),
+        ];
+        for test in &tests {
+            let result = process_op_ra_rb_c2(test.0, test.1).unwrap();
+            assert_eq!(result, test.2);
+        }
+
+        let invalid_tests = [
+            ("test", &Opcode::NOP),
+            ("r1, r3, r3123", &Opcode::NOP),
+            ("r2,,r3 r3", &Opcode::NOP),
+            ("a3,b3", &Opcode::NOP),
+        ];
+        for test in &invalid_tests {
+            let result = process_op_ra_rb_rc(test.0, test.1);
+            assert!(result.is_err(), format!("failed with [{}]", test.0));
+        }
+    }
+
+    #[test]
+    fn process_op_ra_c1_test() {
+        let tests = [
+            ("r1, 5", &Opcode::LDR, 
+            Arguments{ra: Some(1), rb: None, rc: None, c1: Some(5), c2: None, c3: None}),
+            ("  r23,   16", &Opcode::STR, 
+            Arguments{ra: Some(23), rb: None, rc: None, c1: Some(16), c2: None, c3: None}),
+            ("  r23,16", &Opcode::LDR, 
+            Arguments{ra: Some(23), rb: None, rc: None, c1: Some(16), c2: None, c3: None}),
+        ];
+        for test in &tests {
+            let result = process_op_ra_c1(test.0, test.1).unwrap();
+            assert_eq!(result, test.2);
+        }
+
+        let invalid_tests = [
+            ("test", &Opcode::NOP),
+            ("r1, 31a23", &Opcode::NOP),
+            ("r2,3,,,r3", &Opcode::NOP),
+            ("a3,5", &Opcode::NOP),
+        ];
+        for test in &invalid_tests {
+            let result = process_op_ra_rb_rc(test.0, test.1);
+            assert!(result.is_err(), format!("failed with [{}]", test.0));
+        }
+    }
+
+    #[test]
+    fn process_op_ra_rc_test() {
+        let tests = [
+            ("r1, r3", &Opcode::LDR, 
+            Arguments{ra: Some(1), rb: None, rc: Some(3), c1: None, c2: None, c3: None}),
+            ("  r23,   r6", &Opcode::STR, 
+            Arguments{ra: Some(23), rb: None, rc: Some(6), c1: None, c2: None, c3: None}),
+            ("  r23,r31", &Opcode::LDR, 
+            Arguments{ra: Some(23), rb: None, rc: Some(31), c1: None, c2: None, c3: None}),
+        ];
+        for test in &tests {
+            let result = process_op_ra_rc(test.0, test.1).unwrap();
+            assert_eq!(result, test.2);
+        }
+
+        let invalid_tests = [
+            ("test", &Opcode::NOP),
+            ("r1, r31a23", &Opcode::NOP),
+            ("r2,r3,,,r3", &Opcode::NOP),
+            ("a3,r5", &Opcode::NOP),
+        ];
+        for test in &invalid_tests {
+            let result = process_op_ra_rb_rc(test.0, test.1);
+            assert!(result.is_err(), format!("failed with [{}]", test.0));
+        }
     }
 
     #[test]
@@ -302,6 +574,20 @@ mod tests {
             let input = format!("r{}", i);
             let result = register_string_parse(&input).unwrap();
             assert_eq!(i, result);
+        }
+
+        let invalid_tests = [
+            "a23",
+            "123",
+            "",
+            "rrr",
+            "r 23",
+            "r32",
+            "r-2",
+        ];
+        for test in &invalid_tests {
+            let result = register_string_parse(test);
+            assert!(result.is_err(), format!("failed with [{}]", test));
         }
     }
 }     
